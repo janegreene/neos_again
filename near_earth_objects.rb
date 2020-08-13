2046e066adfc9f257277518cfa8ffbd9adb34951
Jane Greene <janegreenecasa@gmail.com>
2020-08-13 17:52:21 -0600

    test and method for formatting data returned

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -24,7 +24,21 @@ class NearEarthObjects
     end
     largest_astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
   end
-  
+
+  def total_number_of_astroids
+    parsed_asteroids_data.length
+  end
+
+  def formatted_astroid_data
+     parsed_asteroids_data.map do |astroid|
+       {
+         name: astroid[:name],
+         diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+         miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+       }
+     end
+  end
+
   def self.find_neos_by_date(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',

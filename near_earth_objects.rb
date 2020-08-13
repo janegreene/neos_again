ff86596792096e288742d0a41f38a45457a5766f
Jane Greene <janegreenecasa@gmail.com>
2020-08-13 17:48:22 -0600

    added test and method for largest asteroid

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -7,6 +7,24 @@ Figaro.application = Figaro::Application.new(environment: 'production', path: Fi
 Figaro.load
 
 class NearEarthObjects
+  attr_reader :parsed_asteroids_data
+  def initialize(date)
+    conn = Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, api_key: ENV['nasa_api_key']}
+    )
+    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+
+    @parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
+
+  def largest_astroid_diameter
+    largest_astroid = parsed_asteroids_data.max_by do |astroid|
+      astroid[:estimated_diameter][:feet][:estimated_diameter_max]
+    end
+    largest_astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+  end
+  
   def self.find_neos_by_date(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
